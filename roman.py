f68accd0cd7840f53d351275ad3a490ebaef1448
DESKTOP-F2D6R8T\Саса <kdobrokhotova03@gmail.com>
2020-11-21 19:30:47 +0300

    Arabic numbers to Roman

--- roman.py
+++ roman.py
@@ -1,13 +1,24 @@
-def num2roman(num):
-
-    roman = ''
-
-    while num > 0:
-        for i, r in num_map:
-            while num >= i:
-                roman += r
-                num -= i
-
-    return roman
-
-num2roman(2242)
+class Additioinall:
+    @staticmethod
+    def int_to_roman(number):
+        ROMAN = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+        result = ""
+        for (arabic, roman) in ROMAN:
+            (factor, number) = divmod(number, arabic)
+            result += roman * factor
+        print(result)
+        return result
